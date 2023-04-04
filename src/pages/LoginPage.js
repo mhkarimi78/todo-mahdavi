3c53f735e81584f2173c53e5d91819e3f5808fee
mhkarimi78 <mahsa_karim16@yahoo.com>
2023-04-04 16:51:15 +0330

    added to login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -7,32 +7,42 @@ import { setUser } from "../store/userInfo";
 
 function LoginPage() {
   const [name, setName] = useState("");
+  const [familyName, setFamilyName] = useState("");
+  const [age, setAge] = useState();
   const dispatch = useDispatch();
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl mb-10">ToDo List</h1>
       <div className="border-2 border-blue-500 p-12 rounded-3xl">
         <BasicTextFields label="name" value={name} setValue={setName} />
-        <BasicTextFields label="familyName" />
-        <BasicTextFields label="age" />
+        <BasicTextFields
+          label="familyName"
+          value={familyName}
+          setValue={setFamilyName}
+        />
+        <BasicTextFields label="age" value={age} setValue={setAge} />
         <BasicButtons
-          label="Login"
+          label="Start"
+          // "Login"
           onClick={() => {
-            dispatch(
-              setUser({
-                name: name,
-                familyName: "",
-                age: null,
-              })
-            );
+            name.length !== 0 &&
+              familyName.length !== 0 &&
+              age > 10 &&
+              dispatch(
+                setUser({
+                  name: name,
+                  familyName: familyName,
+                  age: age,
+                })
+              );
           }}
         />
-        <p className="mt-6 text-center w-full">
+        {/* <p className="mt-6 text-center w-full">
           don't have an account?{" "}
           <NavLink to="/signup" className="underline">
             Sign Up
           </NavLink>
-        </p>
+        </p> */}
       </div>
     </div>
   );
